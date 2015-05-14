348bc52addf96c1eef100670c7c4269e537cbbf9
Debbie <debbalbo@hotmail.com>
2015-05-14 14:06:19 -0700

    getting routing situated and needed to commit something

--- app.rb
+++ app.rb
@@ -6,3 +6,28 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+
+get('/recipes') do
+  @recipes = Recipe.all
+  erb(:recipes)
+end
+
+post('/recipies') do
+  name = params.fetch('name')
+  new_recipe = Recipe.new(name: name)
+  if new_recipe.save()
+    redirect('/recipies')
+  else
+    erb(:error)
+  end
+end
+
+
+get('/recipes/:id') do
+  id = params.fetch('id').to_i()
+  @recipe = Recipe.find(id)
+  @ingredients = @recipe.ingredients()
+  @categories = Category.all()
+  erb(:recipe)
+end
